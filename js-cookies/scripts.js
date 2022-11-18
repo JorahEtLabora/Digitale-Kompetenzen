f95e27719d0afa1d4b78d44ed8a3d5c42f2b6efe
Mabinuel <manuel-kurz02@gmx.de>
2022-11-18 10:05:51 +0100

    Cookie Test

--- js-cookies/scripts.js
+++ js-cookies/scripts.js
@@ -1,5 +1,3 @@
 function myevent() {
-    document.cookie = "isread=true; expires=Sa, 19 Nov 2022 12:00:00 UTC; path=/";
-    var x = document.cookie
-    window.alert(x);
+    window.alert(document.cookie);
 }
