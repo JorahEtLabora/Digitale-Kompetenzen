331e72c1f8a2f66d63bf342f3dd36a9c52114b3e
Jorah Braungardt <jorah.braungardt@gmail.com>
2022-11-23 14:13:59 +0100

    Hinweisbenachrichtigung zu Cookienutzung

--- js-main/modalscript.js
+++ js-main/modalscript.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
     $("#myModal").modal('show');
 });
+
+/* Toasts */
+/* Cookies abgelehnt */
+$(document).ready(function(){
+    $("#cookie-dismiss").click(function(){
+        $("#rejectedToast").toast("show");
+    });
+});
+/* Cookies akzeptiert */
+$(document).ready(function(){
+    $("#cookie-accept").click(function(){
+        $("#acceptedToast").toast("show");
+    });
+});
